2ab8ffaea766149dd286bead07d209794b564ed9
Richter-Anto <richtermullassery@gmail.com>
2022-05-25 11:09:54 +0530

    side bar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
 <v-app>
-  <NavBar />
+  <!-- <NavBar />
   <HeroPart />
   <why-spoty/>
   <FreeSpoty/>
   <GotQue/>
   <LetsPlay/>
-  <FooterSpot/>
+  <FooterSpot/> -->
+  <HomePage />
 </v-app>
   
 </template>
 
 <script>
-import NavBar from "./components/NavBar.vue";
-import HeroPart from "./components/HeroPart.vue";
-import WhySpoty from './components/WhySpoty.vue';
-import FreeSpoty from "./components/FreeSpoty.vue";
-import GotQue from "./components/GotQue.vue";
-import LetsPlay from "./components/LetsPlay.vue";
-import FooterSpot from './components/FooterSpot.vue';
+import HomePage from './pages/HomePage.vue';
+// import NavBar from "./components/NavBar.vue";
+// import HeroPart from "./components/HeroPart.vue";
+// import WhySpoty from './components/WhySpoty.vue';
+// import FreeSpoty from "./components/FreeSpoty.vue";
+// import GotQue from "./components/GotQue.vue";
+// import LetsPlay from "./components/LetsPlay.vue";
+// import FooterSpot from './components/FooterSpot.vue';
 
 
 
 export default {
-  name: "App",
-  data: () => ({
+    name: "App",
+    data: () => ({
     //
-  }),
-  components: { NavBar, HeroPart, WhySpoty, FreeSpoty, GotQue, LetsPlay,FooterSpot},
+    }),
+    components: { HomePage }
 };
 </script>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <SideBar />
+    </div>
+</template>
+
+<script>
+import SideBar from '@/components/SideBar.vue';
+    export default {
+    name: "HomePage",
+    components: { SideBar }
+}
+</script>
+
+<style scoped>
+
+</style>
